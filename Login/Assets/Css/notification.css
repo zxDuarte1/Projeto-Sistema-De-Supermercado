42d647da3d255c8b8193998078dd36805f5791ee
zxDuarte1 <165347640+zxDuarte1@users.noreply.github.com>
2026-05-09 20:43:46 -0300

    Add files via upload

---
+++ Login/Assets/Css/notification.css
@@ -0,0 +1,24 @@
+#toast-container {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  z-index: 9999;
+}
+
+.toast {
+  background: #333;
+  color: white;
+  padding: 14px 20px;
+  margin-top: 10px;
+  border-radius: 8px;
+  min-width: 250px;
+
+  opacity: 0;
+  transform: translateX(100%);
+  transition: 0.4s;
+}
+
+.toast.show {
+  opacity: 1;
+  transform: translateX(0);
+}
